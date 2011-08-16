addd7270edf5eebda01bb5bc266167300ca21f02
Dave Donahue <ddonahue99@gmail.com>
2011-08-16 15:22:52 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CanaryTest::Application.config.secret_token = 'ccdaf0b5c215d280b4bd6a8768a71d64dc02c9c7c146a88d6b25cdd36cce7e2c893e60354b663fb1d64e12dc1b31e68b2d457331734d73186a4acdb89b92b90b'
